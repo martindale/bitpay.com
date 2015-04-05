0bd612ea5a366b5cccb5fa4eca118b63f240a438
Jason Dreyzehner <jason@bitpay.com>
2015-04-05 02:42:05 -0400

    build performance, scripts with 'jademin'

--- gulpfile.js
+++ gulpfile.js
@@ -4,182 +4,59 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var del = require('del');
 var path = require('path');
-var fs = require('fs');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var globby = require('globby');
+var env = {};
 
-var AUTOPREFIXER_BROWSERS = [
-  'ie >= 8',
-  'ff >= 30',
-  'chrome >= 31',
-  'safari >= 5.1',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4',
-  '> 0.25%' //global market share
-];
+gulp.task('default', function(cb) {
+  runSequence('build', cb);
+});
 
-gulp.task('default', ['delete'], function(cb){
+gulp.task('build', ['delete'], function(cb) {
   runSequence(
-    ['build', 'images', 'copy'],
+    ['sass', 'jade', 'images', 'copy'], ['styles', 'jademin'],
     'hash',
     'build-localizations',
-    'html',
     cb);
 });
 
-gulp.task('build', function(cb){
-  runSequence(
-    ['sass', 'jade'],
-    ['styles', 'scripts'],
-    cb);
-});
-
-gulp.task('rebuild-styles', function(cb){
-  runSequence(
-    'sass',
-    'styles',
-    cb);
-});
-
-// delete build and dist
-gulp.task('delete', del.bind(null, ['build', 'dist']));
-
-// Compile & autoprefix styles
-gulp.task('sass', function () {
-  // For best performance, don't add partials to `gulp.src`
-  return gulp.src([
-      'src/styles/**/*.scss'
-    ])
-    .pipe($.sourcemaps.init())
-    .pipe($.changed('build/styles', {extension: '.scss'}))
-    .pipe($.sass({
-        outputStyle: 'expanded',
-        precision: 10,
-        onError: console.error.bind(console, 'Sass error:')
-      }))
-    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('build/styles'))
-    .pipe($.size({title: 'styles'}));
-});
-
-// compile Jade to pretty html in build
-gulp.task('jade', function () {
-  var JADE_LOCALS = {
-    getObjectFromJson: function(path){
-      return JSON.parse(String(fs.readFileSync(path)));
-    }
-  };
-
-  return gulp.src(['src/**/*.jade', '!src/_**/*.jade'])
-    .pipe($.jade({
-      pretty: true,
-      locals: JADE_LOCALS
-    }))
-    .pipe(gulp.dest('build'))
-    .pipe($.size({title: 'jade'}));
-});
-
-
-gulp.task('styles', function () {
-  return gulp.src(['build/**/*.css'])
-    .pipe($.uncss({
-      html: globby.sync(tasks.localization.htmlExcludingLocales)
-    }))
-    .pipe(gulp.dest('build'))
-    .pipe($.csso())
-    .pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'uncss'}));
-});
-
-// Scan HTML for build:js blocks. Clean, concat, minify js
-gulp.task('scripts', ['jshint'], function () {
-  var assets = $.useref.assets({searchPath: '{build,components,src}'});
-
-  return gulp.src(['src/**/*.html'].concat(tasks.localization.htmlExcludingLocales))
-    .pipe(assets)
-    .pipe($.if('*.js', $.uglify()))
-    .pipe(assets.restore())
-    .pipe($.debug())
-    .pipe($.useref())
-    .pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'html'}));
-});
-
-gulp.task('images', function () {
-  return gulp.src('src/images/**/*')
-    .pipe($.cached('images'))
-    .pipe(gulp.dest('build/images'))
-    .pipe($.imagemin({
-      progressive: true,
-      interlaced: true
-    }))
-    .pipe(gulp.dest('dist/images'))
-    .pipe($.size({title: 'images'}));
-});
-
-// Copy files at root level of 'src' to dist
-gulp.task('copy', function () {
-  return gulp.src([
-    'src/*',
-    '!src/_*',
-    '!src/*.jade'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('build'))
-    .pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'copy'}));
-});
-
-//add hashes to filenames to bust caches, write rev-manifest.json
-gulp.task('hash', function() {
-  return gulp.src(['dist/**/*.html', 'dist/**/*.css', 'dist/**/*.js', 'dist/images/**/*'], { base: path.join(process.cwd(), 'dist') })
-    .pipe($.revAll({ ignore: ['.html'], quiet: true }))
-    .pipe(gulp.dest('dist'))
-    .pipe($.revNapkin({verbose: false}))
-    .pipe($.revAll.manifest())
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('html', function() {
-  gulp.src('dist/**/*.html')
-    .pipe($.minifyHtml({conditionals: true}))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('jshint', function () {
-  return gulp.src(['*.js', 'tasks/*.js', 'src/**/*.js'])
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+gulp.task('build:dev', ['delete'], function(cb) {
+  env.development = true;
+  if (env.uncss) {
+    runSequence(
+      ['sass', 'jade:dev', 'images', 'copy'], ['styles', 'build-localizations'],
+      cb);
+  } else {
+    runSequence(
+      ['sass', 'jade:dev', 'images', 'copy'], ['build-localizations'],
+      cb);
+  }
 });
 
 // Watch for changes & reload
-gulp.task('serve', ['default'], function () {
+gulp.task('serve', ['build:dev'], function() {
   browserSync({
     notify: false,
     logPrefix: 'serve',
     server: {
       //serve from build, fall back to src
-      baseDir: ['build', 'components', 'src'],
+      baseDir: ['dist', 'components', 'src'],
       serveStaticOptions: {
         extensions: 'html'
       }
     }
   });
 
-  gulp.watch(['src/**/*.jade','src/**/*.html'], ['build', reload]);
+  gulp.watch(['src/**/*.jade', 'src/**/*.html'], ['rebuild-jade', reload]);
   gulp.watch(['src/{_styles,styles}/**/*.{scss,css}'], ['rebuild-styles', reload]);
   gulp.watch(['*.js', 'tasks/*.js', 'src/**/*.js'], ['jshint']);
-  gulp.watch(['src/images/**/*'], reload);
+  gulp.watch(['src/images/**/*'], ['images'], reload);
 });
 
 // Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], function () {
+gulp.task('serve:dist', ['default'], function() {
   browserSync({
     notify: false,
     logPrefix: 'serve:dist',
@@ -191,6 +68,119 @@ gulp.task('serve:dist', ['default'], function () {
       }
     }
   });
+});
+
+gulp.task('serve:uncss', function(cb) {
+  env.uncss = true;
+  runSequence('serve', cb);
+});
+
+
+gulp.task('rebuild-jade', function(cb) {
+  if (env.uncss) {
+    runSequence(['sass', 'jade:dev'], ['styles', 'build-localizations'], cb);
+  } else {
+    runSequence(['jade:dev'], ['build-localizations'], cb);
+  }
+});
+
+gulp.task('rebuild-styles', function(cb) {
+  if (env.uncss) {
+    runSequence('sass', 'styles', cb);
+  } else {
+    runSequence('sass', cb);
+  }
+});
+
+// delete dist
+gulp.task('delete', del.bind(null, ['dist']));
+
+// Compile & autoprefix styles
+gulp.task('sass', function() {
+  var AUTOPREFIXER_BROWSERS = [
+    'ie >= 8',
+    'ff >= 30',
+    'chrome >= 31',
+    'safari >= 5.1',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4',
+    '> 0.25%' //global market share
+  ];
+
+  // For best performance, don't add partials to `gulp.src`
+  return gulp.src([
+      'src/styles/**/*.scss'
+    ])
+    .pipe($.if(env.development, $.sourcemaps.init()))
+    .pipe($.sass({
+      outputStyle: 'expanded',
+      precision: 10,
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe($.if(env.development, $.sourcemaps.write()))
+    .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src(['dist/**/*.css'])
+    .pipe($.uncss({
+      html: globby.sync(tasks.localization.htmlExcludingLocales)
+    }))
+    .pipe($.if(!env.development, $.csso()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe($.cached('images'))
+    .pipe($.imagemin({
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size({
+      title: 'images'
+    }));
+});
+
+// Copy files at root level of 'src' to dist
+gulp.task('copy', function() {
+  return gulp.src(['src/*', '!src/_*', '!src/*.jade'], {
+      dot: true
+    })
+    .pipe($.cached('copy'))
+    .pipe(gulp.dest('dist'));
+});
+
+//add hashes to filenames to bust caches, write rev-manifest.json
+gulp.task('hash', function() {
+  return gulp.src(['dist/**/*.html', 'dist/**/*.css', 'dist/**/*.js', 'dist/images/**/*'], {
+      base: path.join(process.cwd(), 'dist')
+    })
+    .pipe($.revAll({
+      ignore: ['.html'],
+      quiet: true
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe($.revNapkin({
+      verbose: false
+    }))
+    .pipe($.revAll.manifest())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(['*.js', 'tasks/*.js', 'src/**/*.js'])
+    .pipe($.cached('jshint'))
+    .pipe(reload({
+      stream: true,
+      once: true
+    }))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
 // Load custom tasks from the `tasks` directory
