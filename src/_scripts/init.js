8556593ae341fcdb9b946dc1b164fa5fa36efb4d
Jason Dreyzehner <jason@bitpay.com>
2015-04-24 12:37:16 -0400

    smooth scrolling

--- src/_scripts/init.js
+++ src/_scripts/init.js
@@ -5,4 +5,25 @@
     window.analytics.page();
   });
   InstantClick.init();
+
+
+  //smooth scrolling on clicks
+  jQuery('a[href*=#]').on('click', function(e) {
+    if (this.href.indexOf('#') === 0) {
+      e.preventDefault();
+    }
+    jQuery('body').animate({
+      scrollTop: parseInt(jQuery('' + this.href.substring(this.href.indexOf('#'))).offset().top)
+    }, 500);
+  });
+
+  //smooth scrolling on pageloads
+  jQuery(function() {
+    if (window.location.hash) {
+      jQuery('body').animate({
+        scrollTop: parseInt(jQuery('' + window.location.href.substring(window.location.href.indexOf('#'))).offset().top)
+      }, 500);
+    }
+  });
+
 }());
