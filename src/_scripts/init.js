e01c2d8c2c8d3823574f56d16564fd3e5d297a1c
Jason Dreyzehner <jason@bitpay.com>
2015-04-23 23:05:38 -0400

    add magellan to terms of use

--- src/_scripts/init.js
+++ src/_scripts/init.js
@@ -1,6 +1,14 @@
 (function() {
   'use strict';
   InstantClick.on('change', function() {
+    $(document).foundation({
+      /*jshint camelcase: false */
+      'magellan-expedition': {
+        destination_threshold: 50,
+        offset_by_height: false
+      }
+      /*jshint camelcase: true */
+    });
     window.analytics.page();
   });
   InstantClick.init();
