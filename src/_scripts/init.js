b46896822338b835e6b7e077a24dc42094b7a7cb
Jason Dreyzehner <jason@bitpay.com>
2015-04-24 17:02:42 -0400

    smooth scroll after instantclick change

--- src/_scripts/init.js
+++ src/_scripts/init.js
@@ -2,28 +2,31 @@
   'use strict';
   InstantClick.on('change', function() {
     jQuery(document).foundation();
+    initSmoothScroll();
     window.analytics.page();
   });
+  initSmoothScroll();
   InstantClick.init();
 
-
-  //smooth scrolling on clicks
-  jQuery('a[href*=#]').on('click', function(e) {
-    if (this.href.indexOf('#') === 0) {
-      e.preventDefault();
-    }
-    jQuery('body').animate({
-      scrollTop: parseInt(jQuery('' + this.href.substring(this.href.indexOf('#'))).offset().top)
-    }, 500);
-  });
-
-  //smooth scrolling on pageloads
-  jQuery(function() {
-    if (window.location.hash) {
+  function initSmoothScroll(){
+    //smooth scrolling on clicks
+    jQuery('a[href*=#]').on('click', function(e) {
+      if (this.href.indexOf('#') === 0) {
+        e.preventDefault();
+      }
       jQuery('body').animate({
-        scrollTop: parseInt(jQuery('' + window.location.href.substring(window.location.href.indexOf('#'))).offset().top)
+        scrollTop: parseInt(jQuery('' + this.href.substring(this.href.indexOf('#'))).offset().top)
       }, 500);
-    }
-  });
+    });
+
+    //smooth scrolling on pageloads
+    jQuery(function() {
+      if (window.location.hash) {
+        jQuery('body').animate({
+          scrollTop: parseInt(jQuery('' + window.location.href.substring(window.location.href.indexOf('#'))).offset().top)
+        }, 500);
+      }
+    });
+  }
 
 }());
